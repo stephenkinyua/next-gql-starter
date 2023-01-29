3c3584ac93a5270be1a3379cf31c5d801d86a930
Stephen Kinyua <kinyuamunene.km@gmail.com>
2023-01-29 09:41:08 +0300

    lc: cleanup unused styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
 import { queryClient, getDogs } from "@/api";
 import { dehydrate, useQuery } from "react-query";
 
+// import { Inter } from "@next/font/google";
 // const inter = Inter({ subsets: ["latin"] });
 
 export async function getServerSideProps() {
